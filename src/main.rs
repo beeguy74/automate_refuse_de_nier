d456b42d64467f39feee01718e296e42b8dc37a1
NEVEROV iaroslav <beeguy74@gmail.com>
2025-08-18 18:48:13 +0200

    sdl loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{array, collections::BTreeSet};
 mod tools;
-use tools::{DFA, State, Symbol, parsing};
+use tools::{DFA, State, Symbol, parsing, run};
 
 
 fn main() {
@@ -11,13 +11,14 @@ fn main() {
     //   (q0, 'b') -> q0
     //   (q1, 'a') -> q1
     //   (q1, 'b') -> q0
-    let (states, alphabet, delta_set, start, accept) = parsing();
-    let dfa = DFA::new(
-        states, alphabet, delta_set, start, accept
-    );
+    // let (states, alphabet, delta_set, start, accept) = parsing();
+    // let dfa = DFA::new(
+    //     states, alphabet, delta_set, start, accept
+    // );
 
-    let tests = ["", "a", "w", "t", "wa", "add", "asda", "auia"];
-    for s in tests {
-        println!("{s:5} -> accepts? {}", dfa.accepts(s));
-    }
+    // let tests = ["", "a", "w", "t", "wa", "add", "asda", "auia"];
+    // for s in tests {
+    //     println!("{s:5} -> accepts? {}", dfa.accepts(s));
+    // }
+    run();
 }
--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -2,3 +2,5 @@ pub mod dfa;
 pub use dfa::{DFA, State, Symbol};
 pub mod parsing;
 pub use parsing::parsing;
+pub mod keycatcher;
+pub use keycatcher::run;
