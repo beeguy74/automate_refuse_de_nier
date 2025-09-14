52f89e945774a3911319c6c1f633875e11fc57ad
NEVEROV iaroslav <beeguy74@gmail.com>
2025-09-14 16:48:54 +0200

    play a little with sdl keys

--- src/tools/keycatcher.rs
+++ src/tools/keycatcher.rs
@@ -1,11 +1,35 @@
 extern crate sdl2; 
 
 use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use std::error::Error;
+use sdl2::event::{self, Event};
 use std::time::Duration;
+use sdl2::keyboard::{Keycode, Scancode};
+
+fn parse_key(name: &str) -> Option<Keycode> {
+    // Try a layout-aware key first
+    if let Some(kc) = Keycode::from_name(name) {
+        return Some(kc);
+    }
+    // Fallback: try physical key name, then map to a Keycode
+    Scancode::from_name(name).and_then(Keycode::from_scancode)
+}
+
+
+fn catch_keys(event: Event) -> Result<i32, Box<dyn Error>> {
+    match event {
+        Event::Quit {..} |
+        Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+            Ok(-1)
+        },
+        Event::KeyDown { keycode: Some(key), .. } => {
+            Ok(key.into_i32())
+        },
+        _ => { Ok(-2)}
+    }
+}
  
-pub fn run() {
+pub fn run() -> Result<i32, Box<dyn Error>> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
@@ -14,32 +38,21 @@ pub fn run() {
         .build()
         .unwrap();
  
-    let mut canvas = window.into_canvas().build().unwrap();
- 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
-    'running: loop {
+    'runnin: loop {
         i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                Event::KeyDown { keycode: Some(key), .. } => {
-                    println!("Pressed key{:?}", key)
-                },
-                _ => {}
+            match catch_keys(event) {
+                Ok(key) if key > -1 => { println!("Key is {:?}", key) },  
+                Ok(-1) => { break 'runnin },
+                Ok(_) => {},
+                Err(_) => { return Err("Wrong keys!".into()) }
             }
         }
         // The rest of the game loop goes here...
 
-        canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+    Ok(0)
 }
